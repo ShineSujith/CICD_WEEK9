3b2c8162af25a8890ceb6e6f8246685f1f45e367
Shine Sujith <G00414454@atu.ie>
2024-11-20 14:39:44 +0000

    added jpa and set up h2 database

--- src/main/java/ie/atu/week11example/Person.java
+++ src/main/java/ie/atu/week11example/Person.java
@@ -1,5 +1,9 @@
 package ie.atu.week11example;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
@@ -8,7 +12,12 @@ import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@Entity
 public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     @NotBlank
     private String name;
 
--- src/main/java/ie/atu/week11example/PersonRepository.java
+++ src/main/java/ie/atu/week11example/PersonRepository.java
@@ -1,6 +1,7 @@
 package ie.atu.week11example;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 public interface PersonRepository extends JpaRepository<Person, Long> {
 
